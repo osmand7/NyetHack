dbf7c28ee21964faa6f9e5ae68b32fa73fbb0517
Osmand <x5566x00@gmail.com>
2023-06-13 16:21:19 +0800

    complete chapter 7

--- app/src/main/java/com/example/nyethack/Tavern.kt
+++ app/src/main/java/com/example/nyethack/Tavern.kt
@@ -1,10 +1,36 @@
 package com.example.nyethack
 const val TAVERN_NAME = "Taernyl's Folly"
 fun main() {
-placeOrder()
+placeOrder("shandy,Dragon's Breath,5.91")
+//    placeOrder("elixir,Shirley's Temple,4.12")
 }
-private fun placeOrder(){
+private fun toDragonSpeak(phrase:String)=
+    phrase.replace(Regex("[AEIOUaeiou]")){
+        when(it.value){
+            "A","a" -> "4"
+            "E","e" -> "3"
+            "I","i" -> "1"
+            "O","o" -> "0"
+            "U","u" -> "|_|"
+            else -> it.value
+        }
+    }
+
+private fun placeOrder(menuDate: String) {
     val indexOfApostrophe = TAVERN_NAME.indexOf('\'')
     val tavernMaster = TAVERN_NAME.substring(0 until indexOfApostrophe)
     println("Madrigal speaks with $tavernMaster about their order.")
+
+    val (type,name,price) = menuDate.split(',')
+    //delimiters界定分隔符號，用,分隔menuDate
+    val message = "Madrigal buys a $name ($type) for $price"
+    println(message)
+
+    val phrase = if(name == "Dragon's Breath"){
+        "Madrigal exclaims ${toDragonSpeak("Ah, delicious $name")}"
+    }else{
+        "Madrigal says: Thanks for the $name."
+    }
+    println(phrase)
 }
+
